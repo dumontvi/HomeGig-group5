040f4dd8a1a6f198b0a58ae692db722de4d637f9
Unknown <miguel.teranbenalcazar@mail.utoronto.ca>
2019-11-04 00:47:19 -0500

    Adding static pages and footer
    - Created controller and html for static pages
    - Created footer and its scss
    - Added styling to application.scss
    - Added routes for static pages
    - Changed root route

---
+++ homegig/app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+
+    def home
+    end
+
+    def about
+    end
+
+    def faq
+    end
+
+    def privacy_policy
+    end
+
+    def terms_of_use
+    end
+
+end
--- homegig/config/routes.rb
+++ homegig/config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'offerings#index'
+  root to: 'static_pages#home'
+
+  get 'about', to: 'static_pages#about'
+  get 'faq', to: 'static_pages#faq'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'terms_of_use', to: 'static_pages#terms_of_use'
 
 end
