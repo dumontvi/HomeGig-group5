0867b1aa43767d4677952b12a6d348243dfe64af
dumontvi <dumo1997@gmail.com>
2019-11-21 19:26:40 -0500

    Added all functionality of profile management page

--- homegig/app/models/notification.rb
+++ homegig/app/models/notification.rb
@@ -1,4 +1,4 @@
 class Notification < ApplicationRecord
-    belongs_to :from_user class_name: "User"
-    belongs_to :to_user class_name: "User"
+    belongs_to :from_user class_name: "Users"
+    belongs_to :to_user class_name: "Users"
 end
