018ae127885d43d749b08ddd2eec486466c20d8a
migueltb <miguel.teranbenalcazar@mail.utoronto.ca>
2019-11-23 00:38:02 -0500

    Fix number of notifications on sidebar

--- homegig/app/controllers/application_controller.rb
+++ homegig/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    helper_method :number_of_unread_notifications
+
+    def number_of_unread_notifications
+        Notification.where(["to_user_id = ? and checked = false", "#{current_user.id}"]).order(created_at: :desc).size
+    end
 end
--- homegig/app/helpers/notifications_helper.rb
+++ homegig/app/helpers/notifications_helper.rb
@@ -1,7 +1,2 @@
 module NotificationsHelper
-
-    def number_of_unread_notifications
-        notifications.size
-    end
-
 end
