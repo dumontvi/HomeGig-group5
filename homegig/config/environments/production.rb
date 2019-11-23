3b3d960be9b4d5ffca2661b70344c2a0f2b82066
migueltb <miguel.teranbenalcazar@mail.utoronto.ca>
2019-11-23 18:34:12 -0500

    Adding mailer production settings

--- homegig/config/environments/production.rb
+++ homegig/config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
+  
   # Mailer configuration
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
